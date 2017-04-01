5afdd9abc0d8111d4885ab10e3db7d2eb83ad31d
Dominik Mast <dominik-mast@web.de>
2017-04-01 20:29:13 +0000

    complete

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function (req,res){
-    res.send("Hello World!")
+var months= ["Januar", "Februar", "March", "Arpil", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+app.get("/:test", function (req,res){
+    var unix;
+    var natural;
+    var eingabe = req.params.test;
+    var unixStart ={
+        year: 1970,
+        month: 1,
+        day: 1
+        
+    }
+    
+    if (parseInt(eingabe)){
+        unix =parseInt(eingabe);
+        var date = new Date(unix*1000);
+        var year = date.getFullYear();
+        var month = months[date.getMonth()];
+        var day = date.getDate();
+        natural = " "+month+" "+day+", "+year;
+        
+    } else{
+        var dateUnix = new Date(eingabe).getTime()/1000;
+        
+        var date = new Date(eingabe);
+        var year = date.getFullYear();
+        var month = months[date.getMonth()];
+        var day = date.getDate();
+        natural = " "+month+" "+day+", "+year;
+        
+        if (month == undefined){natural = date;} else{
+            natural = natural;}
+            unix= dateUnix;
+    }
+   
+   
+   res.json({unix: unix, natural: natural});
     
 })
 
